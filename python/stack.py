c31c5095360ec8ad81f3271ea5800f82bb73fa48
Harshvardhan Chauhan <harsh@gumgum.com>
2014-10-09 11:42:38 -0700

    check lenght of stack still not 100% :(

--- python/stack.py
+++ python/stack.py
@@ -1,1 +1,18 @@
 import sys
+
+
+def main():
+  file = open(sys.argv[1],'r')
+  for l in file:
+    stack = l.split()
+    out = []
+    if len(stack):
+      for a in stack:
+        out.append(stack.pop())
+        if len(stack):
+          stack.pop()
+      print " ".join(out)
+
+
+if __name__ == '__main__':
+  main()
