64c23a8633aadcfc74b6a5ddfa3047a55123952d
Harshvardhan Chauhan <harsh@gumgum.com>
2014-10-09 19:38:14 -0700

    bit position solution

---
+++ python/bit_positions.py
@@ -0,0 +1,14 @@
+import sys
+
+def main():
+  file = open(sys.argv[1])
+  for l in file:
+    line = l.rstrip().split(",")
+    binary = bin(int(line[0]))
+    if binary[-int(line[1])] == binary[-int(line[2])]:
+      print "true"
+    else:
+      print "false"
+
+if __name__=="__main__":
+  main()
