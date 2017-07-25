5b049f0df9407e49e335d35e54e9374ab90ab800
Harsh Chauhan <harsh@danale.com>
2017-07-25 16:14:01 -0700

    indentation

--- python/longest_substr_no_repeat_char.py
+++ python/longest_substr_no_repeat_char.py
@@ -3,14 +3,13 @@
 # e.g. "abcabcbb" has "abc" with length 3.
 def substr_len(s):
 
-    i,j,len = 0, 0, 0
+    i, j, len = 0, 0, 0
     charSet = set()
     for ch in s:
         if ch not in charSet:
             charSet.add(ch)
             j += 1
             len = max(len, j-i)
-
         else:
             charSet.remove(ch)
             i += 1
