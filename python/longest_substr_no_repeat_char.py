cb0cdbed260943517bafef64c8f5ab9a617cd038
Harsh Chauhan <harsh@danale.com>
2017-07-26 16:05:20 -0700

    longest substring without repetition

--- python/longest_substr_no_repeat_char.py
+++ python/longest_substr_no_repeat_char.py
@@ -1,19 +1,19 @@
-#!/usr/local/bin/python3
 # This returns the length of longest substring that does not contain repeating character.
 # e.g. "abcabcbb" has "abc" with length 3.
-def substr_len(s):
+# Solution for
+def lengthOfLongestSubstring(s):
+    i, j, maxLen = 0, 0, 0
+    chars = set()
 
-    i, j, len = 0, 0, 0
-    charSet = set()
-    for ch in s:
-        if ch not in charSet:
-            charSet.add(ch)
-            j += 1
-            len = max(len, j-i)
+    while (i < len(s) and j < len(s)):
+        if s[j] not in chars:
+            chars.add(s[j])
+            j = j+1
+            maxLen = max(maxLen, j - i)
         else:
-            charSet.remove(ch)
-            i += 1
-    return len
+            chars.remove(s[i])
+            i = i + 1
+    return maxLen
 
 s = input()
-print(substr_len(s))
+print(lengthOfLongestSubstring(s))
